75b76a333b22060798a04c5cd73f81cc13e7f341
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-08-18 23:31:24 +0530

    done

---
+++ 04_pointer/ptr.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int*ptr ;
+    // // cout<<&ptr<<endl;
+    // int*ptr = new int;
+    // delete ptr;
+    // cout<<*ptr<<endl;
+
+
+    // void*ptr;
+    // int x = 10;
+    // ptr = &x;
+
+    // cout<<ptr<<endl;
+
+    // int* intptr = static_cast<int*>(ptr);
+    // cout<<*intptr<<endl;
+
+     int  a = 10;
+
+        int*ptr = &a;
+        cout <<*ptr<<" "<<a << endl;
+        cout<<&ptr<< "  "<< &a<< endl;
+        cout<<ptr<<endl;
+}
